11c1d91c44f47cceb2bc5e8f0ead9bd26363f385
Raj Shekar <rajshekar.work@gmail.com>
2024-12-22 23:33:13 +0530

    checkout fix

--- storefront/src/app/[countryCode]/(checkout)/checkout/page.tsx
+++ storefront/src/app/[countryCode]/(checkout)/checkout/page.tsx
@@ -33,7 +33,7 @@ export default async function Checkout() {
   return (
     <div className="grid grid-cols-1 small:grid-cols-[1fr_416px] content-container gap-x-40 py-12">
       <Wrapper cart={cart}>
-        <CheckoutForm cart={cart} customer={customer} />
+        {await CheckoutForm({ cart, customer })}
       </Wrapper>
       <CheckoutSummary cart={cart} />
     </div>
